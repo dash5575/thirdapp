ba35e92d18b14e74147b78c40ce07c8c5ea8b5cd
dasharath <dasharathrakhunde55@gmail.com>
2019-07-18 23:46:28 +0530

    Mythirdapp3

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    FirstComponent,
+    SecondComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
